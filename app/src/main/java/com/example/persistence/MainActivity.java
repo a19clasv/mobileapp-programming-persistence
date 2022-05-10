9e2f8d080faedd606f185f5679a642e89eb4ecb8
Claes Svensson <a19clasv@student.his.se>
2022-05-11 00:49:51 +0200

    Declared the added views

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.persistence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button read, write;
+    private TextView display;
+    private EditText countryName, countryNationalDay, countryCapital;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
