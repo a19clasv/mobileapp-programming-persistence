07b09ef04e3fd4ec07d88df613c4c27311b23d32
Claes Svensson <a19clasv@student.his.se>
2022-05-11 01:58:10 +0200

    When the user taps the 'Read' button, all rows should be read from the database and displayed in the TextView. Except that there's an error preventing that

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.persistence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +53,29 @@ public class MainActivity extends AppCompatActivity {
                 database.insert(DatabaseTables.Country.TABLE_NAME, null, values);
             }
         });
+
+        read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayCountries();
+            }
+        });
+    }
+
+    private void displayCountries() {
+        Cursor cursor = database.query(DatabaseTables.Country.TABLE_NAME, null, null, null, null, null, null);
+        String text = "";
+        while (cursor.moveToNext()) {
+            Country country = new Country(
+                    cursor.getString(cursor.getColumnIndexOrThrow(DatabaseTables.Country.COLUMN_NAME_NAME)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(DatabaseTables.Country.COLUMN_NAME_NATIONAL_DAY)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(DatabaseTables.Country.COLUMN_NAME_CAPITAL))
+            );
+            text += "Name: " + country.getName() + "National day: " + country.getNationalDay() + "Capital: " + country.getCapital() + "\n";
+        }
+        cursor.close();
+        display.setText(text);
+        Log.d("MainActivity", "text");
+        Log.d("MainActivity", text + "text");
     }
 }
