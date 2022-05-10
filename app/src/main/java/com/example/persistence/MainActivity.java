645d602b0afca2a3a27b183d5396f5205904e3dd
Claes Svensson <a19clasv@student.his.se>
2022-05-11 01:24:45 +0200

    When the user taps (clicks) the 'Write' button the values in the EditText views are written as a single row to a SQLite database table

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.persistence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     private Button read, write;
     private TextView display;
     private EditText countryName, countryNationalDay, countryCapital;
+
+    private SQLiteDatabase database;
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +30,23 @@ public class MainActivity extends AppCompatActivity {
         countryName = findViewById(R.id.Country_name);
         countryNationalDay = findViewById(R.id.Country_national_day);
         countryCapital = findViewById(R.id.Country_capital);
+
+        databaseHelper = new DatabaseHelper(this);
+        database = databaseHelper.getWritableDatabase();
+
+        write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = countryName.getText().toString();
+                String nationalDay = countryNationalDay.getText().toString();
+                String capital = countryCapital.getText().toString();
+
+                ContentValues values = new ContentValues();
+                values.put(DatabaseTables.Country.COLUMN_NAME_NAME, name);
+                values.put(DatabaseTables.Country.COLUMN_NAME_NATIONAL_DAY, nationalDay);
+                values.put(DatabaseTables.Country.COLUMN_NAME_CAPITAL, capital);
+                database.insert(DatabaseTables.Country.TABLE_NAME, null, values);
+            }
+        });
     }
 }
